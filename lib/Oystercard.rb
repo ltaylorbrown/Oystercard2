6050695600ad866dfed7482da11007666ddf1a4f
ltaylorbrown <lauren2.brown.lb@googlemail.com>
2021-07-27 15:44:10 +0100

    remember stations

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,13 +1,16 @@
 class Oystercard
 attr_reader :balance 
 attr_accessor :journey
+attr_reader :entry_station
 
 DEFAULT_BALANCE = 0
 MAXIMUM_BALANCE = 90
+MINIMUM_FARE = 1
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
     @journey = journey
+    @entry_station = nil 
   end
 
   def top_up(money)
@@ -15,18 +18,17 @@ MAXIMUM_BALANCE = 90
     raise 'Maximum balance exceeded' if @balance > MAXIMUM_BALANCE 
   end 
 
-  def deduct(fare)
-   @balance = @balance -= fare
-  end
-
-  def touch_in
+  def touch_in(station)
     raise 'You cannot ride - you broke' if no_funds
 
     @journey = true
+    @entry_station = station
   end
 
   def touch_out
+    deduct(MINIMUM_FARE)
     @journey = false
+    @entry_station = nil
   end
 
   def in_journey?
@@ -36,9 +38,13 @@ MAXIMUM_BALANCE = 90
   private 
 
   def no_funds
-    return @balance == DEFAULT_BALANCE ? true : false
+    return @balance <= DEFAULT_BALANCE ? true : false
   end
-  
-end
 
+  def deduct(fare)
+   @balance = @balance -= fare
+  end 
+
+end
+# 
 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'Oystercard'
 
 describe Oystercard do
 
+  let(:station){double :station}
 
   it 'default balance of 0' do
     expect(subject.balance).to eq 0
@@ -9,10 +10,6 @@ describe Oystercard do
 
   it 'can respond to top_up' do 
     expect(subject).to respond_to(:top_up).with(1).argument
-  end
-
-  it 'can respond to deduct' do 
-    expect(subject).to respond_to(:deduct).with(1).argument
   end
 
   it 'can top_up card with new balance' do 
@@ -24,39 +21,40 @@ describe Oystercard do
     expect{ subject.top_up(5)}.to raise_error 'Maximum balance exceeded'
   end
 
-  it 'deducts the fare from balance' do 
-    subject.top_up(50)
-    expect{ subject.deduct(2)}.to change {subject.balance}.by(-2)
-  end
-
-  # it 'touches_in for journey' do
-  #   expect(subject).to respond_to(:touch_in)
-  # end
-
-  # it 'touches_out for journey' do 
-  #   expect(subject).to respond_to(:touch_out)
-  # end
-
   it 'is initially not in journey?' do 
     expect(subject).not_to be_in_journey
   end 
 
   it 'is user in journey?' do
     subject.top_up(50)
-    subject.touch_in
+    subject.touch_in(station)
     expect(subject).to be_in_journey
   end
 
  it 'can touch out' do
     subject.top_up(10)
-    subject.touch_in
+    subject.touch_in(station)
     subject.touch_out
     expect(subject).not_to be_in_journey
   end
 
   it '@balance must be at least 1 to touch_in' do
     subject.top_up(0)
-    expect{ subject.touch_in }.to raise_error 'You cannot ride - you broke'
+    expect{ subject.touch_in(station) }.to raise_error 'You cannot ride - you broke'
   end 
+
+  it 'fare deducted once journey complete' do 
+    subject.top_up(50)
+    subject.touch_in(station)
+    expect { subject.touch_out }.to change {subject.balance}.by(-Oystercard::MINIMUM_FARE)
+  end 
+
+  it 'remember the entry station after touch_in' do
+    subject.top_up(50)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq station 
+  end 
+
+
 
 end 
