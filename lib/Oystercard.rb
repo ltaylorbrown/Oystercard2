d1c4a74e260e510f504d43a935547871184b489c
ltaylorbrown <lauren2.brown.lb@googlemail.com>
2021-07-27 09:05:16 +0100

    in journey status. touch in and out

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
 attr_reader :balance 
+attr_accessor :journey
 
 DEFAULT_BALANCE = 0
 MAXIMUM_BALANCE = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
-    
+    @journey = journey
   end
 
   def top_up(money)
@@ -20,15 +21,15 @@ MAXIMUM_BALANCE = 90
 
   def touch_in
     # fail 'You cannot ride - you broke' if @balance < 1
-
+    @journey = true
   end
 
   def touch_out
-    
+    @journey = false
   end
 
   def in_journey?
-    false
+    return @journey ? true : false
   end 
 
 end
